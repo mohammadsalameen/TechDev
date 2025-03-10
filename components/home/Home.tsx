56645bc83f41a66ffa52213459c72b23c22c893b
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-10 16:58:11 +0200

    responsive feature section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './hero/Hero'
 import About from './about/About'
+import Feature from './feature/Feature'
 
 const Home = () => {
   return (
     <div className='overflow-hidden h-[10000px]'>
       <Hero/>
       <About/>
+      <Feature/>
     </div>
   )
 }
